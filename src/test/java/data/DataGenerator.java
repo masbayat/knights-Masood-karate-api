0c1c34220021a48726b0bf05a956d9287a58770f
Masood Bayat <mas.bayat@hotmail.com>
2023-10-18 17:24:38 -0700

    updating code day3

---
+++ src/test/java/data/DataGenerator.java
@@ -0,0 +1,12 @@
+package data;
+
+
+
+public class DataGenerator {
+    public static String getEmail() {
+//        instructor_mohammad_82710@tekschool.us
+        String email = "instructor_mohammad_";
+        int random = (int) (Math.random() * 8921000);
+        return email + random + "@tekschool.us";
+    }
+}
